f5dd8f7b71ec3b5ed820f9b2b7078ded640aac54
James Fryman <james@frymanet.com>
2011-09-01 14:25:14 -0500

    adding documentation for ruby module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,20 @@
-# Class: 
+# Class: ruby
 #
 # Description
+#   This class installs Ruby and Rubygems.
 #
 # Parameters:
+#   This class takes no parameters
 #   
 # Actions:
+#   Install Ruby
 #
 # Requires:
+#   - Class[stdlib]. This is Puppet Labs standard library to include additional methods for use within Puppet. [https://github.com/puppetlabs/puppetlabs-stdlib]
 #
 # Sample Usage:
+#   include ruby
 #
-
 class ruby {
   include stdlib
   
--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -1,16 +1,21 @@
-# Class: 
+# Class: ruby::redhat
 #
 # Description
+#   This class is designed to install latest Ruby and Rubygem packages
+#   on RHEL based systems. 
 #
 # Parameters:
-#   
+#   This class takes no parameters
+#
 # Actions:
+#   This class installs RPMs for RHEL systems.
 #
 # Requires:
+#   This module has no requirements.   
 #
 # Sample Usage:
+#   This method should not be called directly.
 #
-
 class ruby::redhat {
   $packages = ['ruby', 'rubygems', 'ruby-devel', 'ruby-irb', 'ruby-libs', 
                'ruby-rdoc']
