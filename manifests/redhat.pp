3783eb8d3d019ff419499d9d93f25401566b01f8
James Fryman <james@frymanet.com>
2011-09-01 09:15:17 -0500

    adding baseline puppetdoc headers to all files

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -1,3 +1,16 @@
+# Class: 
+#
+# Description
+#
+# Parameters:
+#   
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+
 class ruby::redhat {
   $packages = ['ruby', 'rubygems', 'ruby-devel', 'ruby-irb', 'ruby-libs', 
                'ruby-rdoc']
